177c24d31979a5248738fee8c3d8a01b1fdc4890
ScuroGuardiano <scuroguardiano@users.noreply.github.com>
2023-01-25 18:22:08 +0100

    Added logger

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,35 @@ mod app_state;
 
 use std::sync::Mutex;
 use crate::app_state::AppState;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware::Logger};
 use dbus_interface::DBusInterface;
+use env_logger::Env;
+#[macro_use]
+extern crate log;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    
     let state = AppState { dbus: Mutex::new(DBusInterface::new()) };
     let app_data = web::Data::new(state);
 
-    HttpServer::new(move || {
+    let host = "0.0.0.0";
+    let port = 4444;
+
+
+    let server = HttpServer::new(move || {
         App::new()
         .app_data(web::Data::clone(&app_data))
+        .wrap(Logger::new("%a \"%r\" %s %bB \"%{Referer}i\" \"%{User-Agent}i\" %Ts"))
         .service(
             web::scope("/systemd")
             .service(systemd::routes::load_unit)
             .service(systemd::routes::list_units)
         )
     })
-    .bind(("0.0.0.0", 1337))?
-    .run()
-    .await
+    .bind((host, port))?;
+    info!("Server bound on {}:{}", host, port);
+
+    server.run().await
 }
