f00d270ac0279daef24488e2a10915fc05711034
Kamil J <kamilj@tutanota.com>
2023-01-30 22:30:11 +0100

    Create basic structure and cover /systemd/list-units with tests.

---
+++ tests/script.js
@@ -0,0 +1,68 @@
+import { check, group } from 'k6';
+import http from 'k6/http';
+
+const base_endpoint = 'http://localhost:4444';
+const only200callback = http.expectedStatuses(300);
+
+export default function () {
+    group('systemd', systemd);
+    group('journald', journald);
+}
+
+function systemd() {
+    group('list-units', systemd_list_units);
+    group('load-unit', systemd_load_unit)
+}
+
+function systemd_list_units() {
+    let list_units_res = http.get(base_endpoint + '/systemd' + '/list-units', { responseCallback: only200callback });
+
+    test_if_json(list_units_res);
+
+    check(list_units_res, {
+        'is journald service present': (r) => {
+            const body = r.json();
+
+            return body.some(
+                (unit) => {
+                    if(unit.name == 'systemd-journald.service') {
+                        return true;
+                    }
+                }
+            );
+        },
+    });
+}
+
+function systemd_load_unit() {
+}
+
+function journald() {
+    group('unit-logs', journald_unit_logs);
+}
+
+function journald_unit_logs() {
+}
+
+function test_if_json(res) {
+    check(res, {
+        'is Content-Type set to json': (r) => {
+            const contentType = r.headers['Content-Type'];
+            if(contentType == 'application/json') {
+                return true;
+            } else {
+                console.error('content-type was', contentType);
+                return false;
+            }
+        },
+        'is body a valid json': (r) => {
+            const bodyJsoned = r.json();
+            if(typeof bodyJsoned == 'object') {
+                return true;
+            } else {
+                console.error('was unable to parse body as JSON');
+                return false;
+            }
+        },
+    });
+}
